8bd911e8d187937c336309151d0cef8a4315d789
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2017-01-24 12:20:52 +0200

    form togling added

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var loginForm = document.forms.loginForm,
 	regForm = document.forms.regForm,
-	logLink = loginForm.elements.pass,
-	regLink = regForm.regL;
-console.log(logLink);
-// logLink.addEventListener("click",function(){
-// 	regForm.style.display = "block";
-// });
+	regLink = document.querySelector(".reg_link"),
+	logLink = document.querySelector(".log_link_2");
+
+regLink.addEventListener("click",function(){
+	regForm.style.display = "block";
+	loginForm.style.display="none";
+});
+
+logLink.addEventListener("click",function(){
+	regForm.style.display = "none";
+	loginForm.style.display="block";
+});
+
