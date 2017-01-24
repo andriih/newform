15778ba58f2f56edb8dba149b706df707e664ea1
andriih <andrii.hnatyshyn@gmail.com>
2017-01-24 22:39:49 +0200

    second form validation input fields finished

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 var loginForm = document.forms.loginForm,
 		mail = loginForm.elements.mail,
 		pass = loginForm.elements.pass,
+		msg = document.querySelector(".msg"),
+		regForm = document.forms.regForm,
+		regLink = document.querySelector(".reg_link"),
+		logLink = document.querySelector(".log_link_2"),
+
 	regForm = document.forms.regForm,
-	regLink = document.querySelector(".reg_link"),
-	logLink = document.querySelector(".log_link_2");
+		fname = regForm.elements.fname,
+		mail2 = regForm.elements.mail2,
+		pass2 = regForm.elements.pass2,
+		passConfirm = regForm.elements.c_pass;
+
+
+
+
+
+
+
+
 regLink.addEventListener("click",function(){
 	regForm.style.display = "block";
 	loginForm.style.display="none";
@@ -13,8 +28,6 @@ logLink.addEventListener("click",function(){
 	loginForm.style.display="block";
 });
 loginForm.addEventListener("submit",function(e){
-	if()
-
 	e.preventDefault();
 });
 
@@ -28,10 +41,15 @@ mail.addEventListener("keyup",function(){
 
     if(!re.test(valueMail) ){
     	errPlaceholder.textContent = "Incorrects email input";
+        //return false;
+        //mail.style.boxShadow="0px 0px 21px 0px red";
     }else{
+        //mail.style.boxShadow="none";
     	errPlaceholder.textContent = "";
+
     }
 });
+
 //PASSWORD
 pass.addEventListener("keyup",function(){
 	var parent = this.parentElement,
@@ -46,8 +64,70 @@ pass.addEventListener("keyup",function(){
     }else{
     	errPlaceholder.textContent = "";
     }
-    
 });
+
+///Form registration
+
+regForm.addEventListener("submit",function(e){
+	e.preventDefault();
+});
+
+fname.addEventListener("keyup", function () {
+		var parent = this.parentElement,
+			valueName = this.value.trim(),
+			errPlaceholder = parent.querySelector(".msg");
+		if(valueName.length <= 3){
+			errPlaceholder.textContent = "Name must be longer then 3 chars";
+		}else{
+			errPlaceholder.textContent = "";
+
+		}
+});
+mail2.addEventListener("keyup",function(){
+    var parent = this.parentElement,
+        valueMail = this.value.trim(),
+        errPlaceholder = parent.querySelector(".msg");
+
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(!re.test(valueMail) ){
+        errPlaceholder.textContent = "Incorrects email input";
+        //return false;
+        //mail.style.boxShadow="0px 0px 21px 0px red";
+    }else{
+        //mail.style.boxShadow="none";
+        errPlaceholder.textContent = "";
+
+    }
+});
+pass2.addEventListener("keyup",function(){
+    var parent = this.parentElement,
+        valuePass = this.value.trim(),
+        errPlaceholder = parent.querySelector(".msg");
+    re =  /^[A-Za-z][A-Za-z0-9]*$/;
+
+    if(!re.test(valuePass)){
+        errPlaceholder.textContent = " only english letters allowed";
+    }else if(valuePass.length <= 3){
+        errPlaceholder.textContent = "password to short";
+    }else{
+    	//return truePass = valuePass;
+    	//console.log(valuePass);
+        errPlaceholder.textContent = "";
+    }
+});
+passConfirm.addEventListener("keyup",function(){
+    var parent = this.parentElement,
+        valuePass = this.value.trim(),
+        errPlaceholder = parent.querySelector(".msg");
+
+
+
+});
+
+
+
+
 
 
 
