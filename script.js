6877e09a94215a93f79c7934d8b30058b0912b46
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2017-01-24 12:03:09 +0200

    init

---
+++ script.js
@@ -0,0 +1,8 @@
+var loginForm = document.forms.loginForm,
+	regForm = document.forms.regForm,
+	logLink = loginForm.elements.pass,
+	regLink = regForm.regL;
+console.log(logLink);
+// logLink.addEventListener("click",function(){
+// 	regForm.style.display = "block";
+// });
